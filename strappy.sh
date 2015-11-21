0419a0173fe098ae93c56e6a8db1d4c74430c5d3
Brandon <brandonmowat11@gmail.com>
2015-11-21 12:50:50 -0500

    fixed css starter code bug

--- strappy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "Creating Website..."
-echo "making directories..."
-mkdir {css,js,img}
-printf "<html>\n<head>\n<title></title>\n</head>\n\n<body>\n</body>\n</html>\n" > index.html
